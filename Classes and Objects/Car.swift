ed45b93bdf1661422d71a67c39baefb2eb2d89be
Steven Saunders <stevensaunders@Stevens-MacBook-Pro.local>
2019-03-04 15:20:28 -0700

    Initial commit for Classes and Objects practice

---
+++ Classes and Objects/Car.swift
@@ -0,0 +1,43 @@
+//
+//  Car.swift
+//  Classes and Objects
+//
+//  Created by Steven Saunders on 3/3/19.
+//  Copyright © 2019 Steven Saunders. All rights reserved.
+//
+
+import Foundation
+
+enum CarType {
+    
+    case Sedan
+    case Coupe
+    case Hatchback
+    case Convertible
+    
+}
+
+class Car {
+    
+    var color = "White"
+    var numberOfSeats = 5
+    var typeOfCar : CarType = .Coupe
+    
+    init() {
+
+    }
+
+    convenience init (customerColorChoice : String, customerNumberOfSeatsChosen : Int, customerTypeOfCarChoice : CarType) {
+        self.init()
+        color = customerColorChoice
+        numberOfSeats = customerNumberOfSeatsChosen
+        typeOfCar = customerTypeOfCarChoice
+    }
+    
+    func drive () {
+    
+        print("Car is moving")
+        
+    }
+    
+}
---
+++ Classes and Objects/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Steven Saunders on 3/3/19.
+//  Copyright © 2019 Steven Saunders. All rights reserved.
+//
+
+import Foundation
+
+let myCar = Car(customerColorChoice: "Gold", customerNumberOfSeatsChosen: 2, customerTypeOfCarChoice: .Coupe)
+
+myCar.drive()
+
+
